5aaf000f999d83ebda3bbb2798f3fbf3b56c0aef
ahm0xc <codingwebsa@gmail.com>
2025-02-16 17:45:31 +0600

    init shadcn

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import { Button } from "~/components/ui/button";
 
 export default function Popup() {
-  return <div>Popup</div>;
+  return (
+    <div className="w-[464px] p-4">
+      <Button>Hello</Button>
+    </div>
+  );
 }
